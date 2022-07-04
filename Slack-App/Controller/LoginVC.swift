1e3abd6cf2fba944a89d355b059cb5d551569a75
DaiBoonz <ngocdaiute@gmail.com>
2022-07-04 16:39:23 +0700

    done lession-04

--- Slack-App/Controller/LoginVC.swift
+++ Slack-App/Controller/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ChannelVC.swift
+//  LoginVC.swift
 //  Slack-App
 //
 //  Created by MacOS-BigSur on 04/07/2022.
@@ -7,20 +7,16 @@
 
 import UIKit
 
-class ChannelVC: UIViewController {
-    
-    @IBOutlet weak var loginBtn: UIButton!
-    
+class LoginVC: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
-     
+
+        // Do any additional setup after loading the view.
     }
     
-    @IBAction func loginBtnPressed(_ sender: Any) {
-        
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
     /*
---
+++ Slack-App/Ultilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Slack-App
+//
+//  Created by MacOS-BigSur on 04/07/2022.
+//
+
+import Foundation
+
+//Segue
+
+let TO_LOGIN = "toLogin"
